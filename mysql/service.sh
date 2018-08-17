cb434f1a1b1aeb4b822b91096e23f896f09b0209
jesse.li <lsj86@qq.com>
2018-08-17 16:00:40 +0800

    mysql

--- mysql/service.sh
+++ mysql/service.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+dir=$(pwd)
+
 case $1 in
 run)
-    docker run --name mysql -v /data/mysql:/var/lib/mysql -v ./custom.cnf:/etc/mysql/conf.d/custom.cnf -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
+    docker run --name mysql -v /data/mysql:/var/lib/mysql -v $dir/custom.cnf:/etc/mysql/conf.d/custom.cnf -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
     ;;
 start)
     docker start mysql
